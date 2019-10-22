2f455737c6cf055d896a95807f63bdb2c127f298
Arnau <arnaucat@gmail.com>
2019-10-22 23:02:39 +0200

    Create serialarduino.py

---
+++ serialarduino.py
@@ -0,0 +1,13 @@
+import serial
+
+arduino = serial.Serial('/dev/ttyACM0', 9600)
+
+while True:
+      coman = raw_input('INSERTAR  [1/0]: ') 
+      arduino.write(coman)
+      if coman == '1':
+            print('LED ON')
+      elif coman == '0':
+            print('LED OFF')
+
+arduino.close()
