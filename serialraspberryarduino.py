8df4173eb719a9ca13230da9486455cf682a6f0c
Arnau <arnaucat@gmail.com>
2019-10-23 16:51:55 +0200

    Update serialraspberryarduino.py

--- serialraspberryarduino.py
+++ serialraspberryarduino.py
@@ -3,7 +3,7 @@ import serial
 arduino = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
-      coman = raw_input('INSERTAR  [1/0]: ') 
+      coman = raw_input('INSERTAR  [1/0]:') 
       arduino.write(coman)
       if coman == '1':
             print('LED ON')
